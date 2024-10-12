92f9b2c3efcc7360b86d694b1d6de33730be05ff
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2024-10-13 02:19:21 +0530

    Designed Header Component.

--- excel-to-queries/src/App.js
+++ excel-to-queries/src/App.js
@@ -1,6 +1,8 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <div>Hello, World!</div>
+    <Header />
   );
 }
 
---
+++ excel-to-queries/src/components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <nav className="navbar bg-primary" data-bs-theme="dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Excel to Queries</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
